16e493abd066663cfa4d69f19be5292b6fa40bdf
Demonus <pogon.ap@hotmail.com>
2017-11-22 18:13:40 +0000

    Article model setup

--- GroupCoursework/Newspaper/models.py
+++ GroupCoursework/Newspaper/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    # The title of the Article
+    title = models.CharField(max_length=255)
+    # The author of the Article
+    author = models.CharField(max_length=255)
+    # The actual article content
+    content = models.TextField()
+    # Publication date
+    pub_date = models.DateTimeField('publication date')
+    # The category of the article
+    category = models.CharField(max_length=255)
+    # The amount of likes for the article
+    likes = models.IntegerField()
+
+
