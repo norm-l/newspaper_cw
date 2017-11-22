79c1aaacd84b0d8c85d2a8d9464a2643ecdcb267
Ageeban Kobalasingam <a.kobalasingam@se14.qmul.ac.uk>
2017-11-22 19:22:50 +0000

    Update models.py

--- GroupCoursework/Newspaper/models.py
+++ GroupCoursework/Newspaper/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -17,4 +18,21 @@ class Article(models.Model):
     # The amount of likes for the article
     likes = models.IntegerField()
 
+
+class User(models.Model):
+    #default user model
+    user = models.OneToOneField(User, related_name='user')
+    #custom name 
+    fullName = models.TextField(default='', blank=True)
+    #custom phone number 
+    phone = models.CharField(max_length=20, blank=True, default='')
+
+
+class Comments (models.Model):
+    #FK User
+    user = models.ForeignKey('User')
+    #FK Article
+    article = models.ForeignKey('Article')
+    #User comment
+    commentContent = models.TextField()
 
