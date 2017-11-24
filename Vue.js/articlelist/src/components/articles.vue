6871e6ceabee9898e25e50866862387816153b4b
Lukas <l.normantas@se14.qmul.ac.uk>
2017-11-24 17:55:50 +0000

    Centered articles + cleaned/commented code

--- Vue.js/articlelist/src/components/articles.vue
+++ Vue.js/articlelist/src/components/articles.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="articles">
     <h1 v-text="title"></h1>
-    <div v-if="Object.keys(articles).length !== 0" class="col-md-10 center-block">
-      <div v-for="article in articles" class="article-border">
+    <!-- Check if there are any articles in the object. -->
+    <div v-if="Object.keys(articles).length !== 0" class="center-block">
+      <!-- Loop through the object finding all articles, attaching the classes to them -->
+      <div v-for="article in articles" class="col-md-10 article-border">
+        <!-- Article Title -->
         <h1 v-text="article.title"></h1>
-        <!-- <img :src="article.image" class="pull-left img-responsive thumb margin10 img-thumbnail"> -->
-        <p v-text="article.content">
-        </p>
-        <!-- <a class="btn btn-readmore pull-right marginBottom10" :href="article.url">Continue Reading..</a> -->
+        <!-- Article Image -->
+        <img :src="article.image" class="pull-left img-responsive margin10 thumb img-thumbnail">
+        <!-- Article Content/Summary -->
+        <p v-text="article.content"></p>
+        <!-- Continue reading the full article -->
+        <a class="btn btn-success pull-right marginBottom10" :href="article.url">Continue Reading..</a>
       </div>
     </div>
+    <!-- No articles were found therefore we display a simple message -->
     <div v-else>No Articles Found!</div>
   </div>
 </template>
@@ -20,6 +26,7 @@
     data() {
       return {
         title: 'Article List',
+        // Placeholder object with a few articles.
         articles: {
           article1: {title: 'Article Title 1', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum hendrerit consequat leo fermentum condimentum. Fusce in eros malesuada, tincidunt felis eget, dignissim nisl. Integer vitae auctor nibh. Integer accumsan mauris eu nulla interdum scelerisque. Sed lacinia vitae augue et aliquet. Suspendisse ultrices felis in turpis consequat, sed pellentesque augue pellentesque. Aliquam erat volutpat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis in enim quis est condimentum finibus. Suspendisse quis lacus dolor. Morbi ut enim eu lorem varius fringilla.', url: '', image: './src/assets/default.png'},
           article2: {title: 'Article Title 2', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum hendrerit consequat leo fermentum condimentum. Fusce in eros malesuada, tincidunt felis eget, dignissim nisl. Integer vitae auctor nibh. Integer accumsan mauris eu nulla interdum scelerisque. Sed lacinia vitae augue et aliquet. Suspendisse ultrices felis in turpis consequat, sed pellentesque augue pellentesque. Aliquam erat volutpat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis in enim quis est condimentum finibus. Suspendisse quis lacus dolor. Morbi ut enim eu lorem varius fringilla.', url: '', image: './src/assets/default.png'},
@@ -33,25 +40,18 @@
 </script>
 
 <style scoped>
+/* To display borders between articles and align all article text left */
 .article-border {
   border-bottom: 1px solid #ddd;
   text-align: left;
 }
 .center-block {
-    float: none;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 }
-.btn-readmore {
-    color: #ffffff;
-    background-color: #37d980;
-    border-color: #37d980;
-    border-radius: 0;
-    margin-bottom: 10px
-}
-.open .dropdown-toggle.btn-blog {
-    color: white;
-    background-color: #34ca78;
-    border-color: #34ca78;
-}
+/* Add a margin to the article image */
 .margin10 {
    margin-bottom: 10px;
    margin-right: 10px;
