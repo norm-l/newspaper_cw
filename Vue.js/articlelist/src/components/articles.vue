6e16f3f0f3a46ae5dad280a83727274af28dda86
Lukas <l.normantas@se14.qmul.ac.uk>
2017-11-26 04:38:23 +0000

    Article tags/button is now positioned correctly, regardless of text #

--- Vue.js/articlelist/src/components/articles.vue
+++ Vue.js/articlelist/src/components/articles.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="articles">
-    <!-- <h1 v-text="title" /> -->
     <!-- Check if there are any articles in the object. -->
     <div v-if="Object.keys(articles).length !== 0" class="center-block">
       <!-- Loop through the object finding all articles, attaching the classes to them -->
       <div v-for="article in articles" class="col-md-10 article-border">
         <!-- Article Title -->
-        <h2 class="margin-top" v-text="article.title" />
+        <h2 class="mt-3" v-text="article.title" />
         <!-- Article Information -->
         <p>
           <icon name="calendar" class="mr-1"/>
@@ -14,10 +13,15 @@
           <icon name="user" class="ml-1 mr-1"/>
           <small class="text-muted" v-text="article.author"/>
         </p>
-        <!-- Article Image -->
-        <img :src="article.image" class="float-left img-responsive mr-3 thumb img-thumbnail">
-        <!-- Article Content/Summary -->
-        <p v-text="article.content" />
+        <!-- Article Content -->
+        <div class="row">
+          <div class="col-md-12">
+            <!-- Article Image -->
+            <img :src="article.image" class="float-left img-responsive mr-3 thumb img-thumbnail">
+            <!-- Article Summary -->
+            <p v-text="article.content" />
+          </div>
+        </div>
         <!-- Continue reading the full article -->
         <a class="btn btn-success float-right mb-3" :href="article.url">Continue Reading..</a>
         <!-- Article Tags -->
@@ -40,7 +44,7 @@
         title: 'Article List',
         // Placeholder object with a few articles.
         articles: {
-          article1: {title: 'Article Title 1', author: 'Author1', date: 'February 2, 2017', content: 'TODO: Make the image/text a block? Otherwise button/tags move when there is less text like so:', url: '', image: './src/assets/default.png', tags: ['finance', 'stocks']},
+          article1: {title: 'Article Title 1', author: 'Author1', date: 'February 2, 2017', content: 'Small text example', url: '', image: './src/assets/default.png', tags: ['finance', 'stocks']},
           article2: {title: 'Article Title 2', author: 'Author2', date: 'February 12, 2017', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum hendrerit consequat leo fermentum condimentum. Fusce in eros malesuada, tincidunt felis eget, dignissim nisl. Integer vitae auctor nibh. Integer accumsan mauris eu nulla interdum scelerisque. Sed lacinia vitae augue et aliquet. Suspendisse ultrices felis in turpis consequat, sed pellentesque augue pellentesque. Aliquam erat volutpat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis in enim quis est condimentum finibus. Suspendisse quis lacus dolor. Morbi ut enim eu lorem varius fringilla.', url: '', image: './src/assets/default.png', tags: ['health', 'research']},
           article3: {title: 'Article Title 3', author: 'Author3', date: 'February 20, 2017', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum hendrerit consequat leo fermentum condimentum. Fusce in eros malesuada, tincidunt felis eget, dignissim nisl. Integer vitae auctor nibh. Integer accumsan mauris eu nulla interdum scelerisque. Sed lacinia vitae augue et aliquet. Suspendisse ultrices felis in turpis consequat, sed pellentesque augue pellentesque. Aliquam erat volutpat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis in enim quis est condimentum finibus. Suspendisse quis lacus dolor. Morbi ut enim eu lorem varius fringilla.', url: '', image: './src/assets/default.png', tags: ['technology', 'ai']}
         }
@@ -57,21 +61,11 @@
   border-bottom: 1px solid #ddd;
   text-align: left;
 }
+/* In order to center all articles */
 .center-block {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-}
-/* Add a margin to the article image */
-.margin10 {
-   /*margin-bottom: 10px;*/
-   margin-right: 10px;
-}
-.margin-bottom {
-  margin-bottom: 10px;
-}
-.margin-top {
-  margin-top: 10px;
 }
 </style>
