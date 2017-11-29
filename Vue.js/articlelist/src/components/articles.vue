d30aaa895f14d7749778a032b5711227c208ae3f
Lukas <l.normantas@se14.qmul.ac.uk>
2017-11-29 20:35:20 +0000

    Modified the articles to display additionally: 1) Category in the header 2) Likes Temporaly changed so that tags are not fetched as a list because the backend currently does not support this

--- Vue.js/articlelist/src/components/articles.vue
+++ Vue.js/articlelist/src/components/articles.vue
@@ -6,54 +6,61 @@
       <div v-for="article in articles" class="col-md-10 article-border">
         <!-- Article Title -->
         <h2 class="mt-3" v-text="article.title" />
-        <!-- Article Information -->
+        <!-- Article Header -->
         <p class="text-muted">
           <icon name="calendar" class="mr-1"/>
-          <small v-text="article.date"/>
+          <small v-text="article.pub_date"/>
           <icon name="user" class="ml-1 mr-1"/>
           <small v-text="article.author"/>
+          <icon name="bookmark" class="ml-1 mr-1"/>
+          <small v-text="article.category"/>
         </p>
         <!-- Article Content -->
         <div class="row">
           <div class="col-md-12">
             <!-- Article Image -->
-            <img :src="article.image" class="float-left img-responsive mr-3 thumb img-thumbnail">
+            <img :src="article.article_img" class="float-left img-responsive mr-3 thumb img-thumbnail">
             <!-- Article Summary -->
             <p v-text="article.content" />
           </div>
         </div>
         <!-- Continue reading the full article -->
-        <a class="btn btn-success float-right mb-3" :href="article.url">Continue Reading..</a>
-        <!-- Article Tags -->
-        <p class="float-left mt-3">
-          <icon name="tags"/>
-          <span v-for="tag in article.tags" class="badge badge-info ml-1" v-text="tag"/>
-        </p>
-      </div>
+        <a class="btn btn-success float-right mb-3" href="#">Continue Reading..</a>
+        <ul class="list-unstyled mt-2">
+          <!-- Article Tags -->
+          <li>
+            <icon name="tags"/>
+            <span class="badge badge-info ml-1" v-text="article.tags"/>
+          </li>
+          <!-- Article Likes -->
+          <li>
+            <icon name="thumbs-up"/>
+            <span class="badge badge-success ml-1" v-text="article.likes"/>
+          </li>
+        </ul>
     </div>
+  </div>
     <!-- No articles were found therefore we display a simple message -->
-    <div v-else>No Articles Found!</div>
+    <div v-else>
+      <icon scale="5" name="warning" class="mt-5"/>
+      <p><b>No Articles Found!</b>
+      <br />Have you added some in <a href="http://127.0.0.1:8000/admin/Newspaper/article/"><i>here</i></a>?</p>
+    </div>
   </div>
 </template>
 
+<!-- <span v-for="tag in article.tags" class="badge badge-info ml-1" v-text="tag"/> When a list is implemented -->
+
 <script>
 import axios from 'axios';
-
   export default {
     name: 'articles',
     data () {
       return {articles:[]}
-      // return {
-      //   // Placeholder object with a few articles.
-      //   articles: {
-      //     article1: {title: 'Article Title 1', author: 'Author1', date: 'February 2, 2017', content: 'Small text example', url: '', image: './static/Newspaper/img/default.png', tags: ['finance', 'stocks']},
-      //     article2: {title: 'Article Title 2', author: 'Author2', date: 'February 12, 2017', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum hendrerit consequat leo fermentum condimentum. Fusce in eros malesuada, tincidunt felis eget, dignissim nisl. Integer vitae auctor nibh. Integer accumsan mauris eu nulla interdum scelerisque. Sed lacinia vitae augue et aliquet. Suspendisse ultrices felis in turpis consequat, sed pellentesque augue pellentesque. Aliquam erat volutpat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis in enim quis est condimentum finibus. Suspendisse quis lacus dolor. Morbi ut enim eu lorem varius fringilla.', url: "./static/Newspaper/img/default.png", image: 'default.png', tags: ['health', 'research']},
-      //     article3: {title: 'Article Title 3', author: 'Author3', date: 'February 20, 2017', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum hendrerit consequat leo fermentum condimentum. Fusce in eros malesuada, tincidunt felis eget, dignissim nisl. Integer vitae auctor nibh. Integer accumsan mauris eu nulla interdum scelerisque. Sed lacinia vitae augue et aliquet. Suspendisse ultrices felis in turpis consequat, sed pellentesque augue pellentesque. Aliquam erat volutpat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis in enim quis est condimentum finibus. Suspendisse quis lacus dolor. Morbi ut enim eu lorem varius fringilla.', url: '', image: './static/Newspaper/img/default.png', tags: ['technology', 'ai']}
-      //   }
-      // }
+      // For local use:
+      // return {articles:[{title: 'title1', author: 'author1', pub_date: 'February 2, 2017', content: 'Small text example', category: 'business', likes: 10, article_img: './src/assets/default.png', tags: 'test'}]}
     },
     mounted: function() {
-      console.log("mounted has been run");
       axios.get("/api/latestarticles")
       .then(response => {this.articles = response.data})
       .catch((err) => {
@@ -63,8 +70,6 @@ import axios from 'axios';
     methods: {
     }
   }
-
-
 </script>
 
 <style scoped>
