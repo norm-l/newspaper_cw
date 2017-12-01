d07b8585674fe5919d1d689e88e72bf4ffdca338
Ageeban Kobalasingam <a.kobalasingam@se14.qmul.ac.uk>
2017-12-01 15:18:36 +0530

    Update serializers.py

--- GroupCoursework/Newspaper/serializers.py
+++ GroupCoursework/Newspaper/serializers.py
@@ -1,9 +1,14 @@
 # serializers.py holds the Rest Framework serializers that are used for our Newspaper App
 
 from rest_framework import serializers
-from .models import Article
+from .models import Article,User
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = ('title' , 'author' , 'content' , 'article_img' ,  'pub_date' , 'category' , 'likes' , 'tags')
+        
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
