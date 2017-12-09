fdcd05da9633494b38950c37d61c680c34cf6176
ec14088 <a.pogonowski@se14.ac.uk>
2017-12-09 19:47:27 +0000

    Fixed the model likes

--- GroupCoursework/Newspaper/serializers.py
+++ GroupCoursework/Newspaper/serializers.py
@@ -6,12 +6,12 @@ from .models import Article,User,Comments,Likes
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ('id' , 'title' , 'author' , 'content' , 'article_img' ,  'pub_date' , 'category' , 'likes' , 'tags')
+        fields = ('id' , 'title' , 'author' , 'content' , 'article_img' ,  'pub_date' , 'category' , 'tags')
 
 class ArticleHeadlineSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ('id' , 'title' , 'author' , 'headline' , 'article_img' ,  'pub_date' , 'category' , 'likes' , 'tags')
+        fields = ('id' , 'title' , 'author' , 'headline' , 'article_img' ,  'pub_date' , 'category' , 'tags')
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
