e62afc2114476883be719459828353ab69a81710
Lukas <l.normantas@se14.qmul.ac.uk>
2017-12-09 00:02:38 +0000

    Fixed formatting issues

--- GroupCoursework/Newspaper/views.py
+++ GroupCoursework/Newspaper/views.py
@@ -1,24 +1,24 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse
-from django.contrib.auth import authenticate,login
-from rest_framework import status
-from rest_framework.decorators import api_view, parser_classes
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from Newspaper.data import *
-from Newspaper.serializers import ArticleSerializer,RegisterSerializer
-from rest_framework.parsers import JSONParser
-import json
-import ast
-from django.views.decorators.csrf import csrf_exempt, csrf_protect
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from Newspaper.serializers import ArticleSerializer, RegisterSerializer
+# UNUSED:
+# from django.http import HttpResponse
+# from rest_framework import status
+# from Newspaper.data import *
+# from rest_framework.parsers import JSONParser
+# from django.views.decorators.csrf import csrf_exempt, csrf_protect
+# from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
 # Create your views here.
+
 def index(request):
     return render(request, 'Newspaper/index.html')
 
 
 def article(request):
-	return render(request,'Newspaper/single.html',{'data':request.POST})
+    return render(request, 'Newspaper/single.html', {'data': request.POST})
 
 # Web API
 
@@ -32,7 +32,7 @@ def get_articles(request):
 
         query = ExtractCategory(request.query_params)
         articles = None
-        if(query != None):
+        if query != None:
             # We got query Parameters
             articles = GetLatestArticlesByCategory(query)
         else:
@@ -40,7 +40,7 @@ def get_articles(request):
             articles = GetAllArticles()
             serializer = ArticleSerializer(articles, many=True)
 
-        serializer = ArticleSerializer(articles, many = True)
+        serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data)
 
 
@@ -53,7 +53,7 @@ def get_latest_articles(request):
 
         query = ExtractCategory(request.query_params)
         articles = None
-        if(query != None):
+        if query != None:
             # We got query Parameters
             articles = GetLatestArticlesByCategory(query)
         else:
@@ -61,7 +61,7 @@ def get_latest_articles(request):
             articles = GetLatestArticles()
             serializer = ArticleSerializer(articles, many=True)
 
-        serializer = ArticleSerializer(articles, many = True)
+        serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data)
 
 
@@ -72,14 +72,13 @@ def get_article(request, pk):
     """
 
     try:
-        if(request.method == 'GET'):
+        if request.method == 'GET':
             article = GetArticleById(pk)
             serializer = ArticleSerializer(article, many=False)
             return Response(serializer.data)
     except:
         return Response(status=400)
 
-    
 
 @api_view(['POST'])
 # @authentication_classes((SessionAuthentication, BasicAuthentication))
@@ -91,31 +90,32 @@ def authentication(request):
     email = request.user.name
     password = request.user.password
     print("logging in with:", email, "|", password)
-    user = authenticate(username=email,password=password)
+    user = authenticate(username=email, password=password)
 
     if user.is_authenticated():
-	    login(request,user)
-	    return redirect('/')
+        login(request, user)
+        return redirect('/')
     else:
-        return render(request,'index.html',{'errors':'user is not defined'})
+        return render(request, 'index.html', {'errors': 'user is not defined'})
+
 
 @api_view(['POST'])
 def register(request):
     data = request.data
     serializer = RegisterSerializer(data=data)
     if serializer.is_valid():
-	    serializer.save()
+        serializer.save()
     else:
-	    return Response(status=500)
+        return Response(status=500)
     return Response(status=200)
 
 
 @api_view(['POST'])
 def comment(request):
-    data=request.data
+    data = request.data
     serializer = CommentSerializer(data=data)
     if serializer.is_valid():
-	serializer.save()
+        serializer.save()
     else:
-	return Response(status=500)
+        return Response(status=500)
     return Response(status=200)
