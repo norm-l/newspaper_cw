f9f8f3933a90bf100825d05c877087a61aa98d7c
ec14088 <a.pogonowski@se14.ac.uk>
2017-12-10 18:11:40 +0000

    merging changes and adding the comments component

--- GroupCoursework/Newspaper/admin.py
+++ GroupCoursework/Newspaper/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 # Register your models here.
 
-from .models import Article, User
+from .models import Article, User, Comments, Likes
 
 admin.site.register(Article)
 admin.site.register(User)
+admin.site.register(Comments)
+admin.site.register(Likes)
