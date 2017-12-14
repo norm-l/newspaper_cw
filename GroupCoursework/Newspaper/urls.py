f8f420fdb9206dd3a9fe044824af0cbbe8ad045d
Lukas <l.normantas@se14.qmul.ac.uk>
2017-12-14 00:20:44 +0000

    tidied urls

--- GroupCoursework/Newspaper/urls.py
+++ GroupCoursework/Newspaper/urls.py
@@ -8,10 +8,8 @@ urlpatterns = [
     url(r'^api/articles/$', views.get_articles, name='get_articles'),
     url(r'^api/latestarticles/$', views.get_latest_articles, name='get_latest_articles'),
     url(r'^api/article/(?P<pk>[0-9]*)', views.get_article, name='get_article'),
-    # url(r'^login$', views.authentication, name='login'),
     url(r'^register$', views.register, name='register'),
     url(r'^comment$', views.comment, name='comment'),
     url(r'^login$', authviews.obtain_auth_token),
-
     url(r'^like/(?P<id>[\d]+)/$', views.like, name='like'),
 ]
